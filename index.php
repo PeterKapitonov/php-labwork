3a9febf30d8d7209848e2c5b5ca9844725e97592
PeterKapitonov <79373925842@mail.ru>
2018-05-09 19:19:04 +0200

    lesson10.Задачи на формы в PHP

--- index.php
+++ index.php
@@ -7,100 +7,119 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<?php 
-		
-//----------------------------------------------------TASK1                                                       
-	echo '<h3>Практика на комбинации функций</h3><br><span>task1</span><div class="window">';
-		$arr = array(1, 2, 3, 4, 5);
-		var_dump($arr);
-		echo '<br>';
-		echo array_sum($arr) / count($arr);
-	echo '</div><br>';
-	
-
-
-//--------------------------------------------------TASK2                                                       
-	echo '<span>task2/сумма чисел от 1 до 100</span><div class="window">';
-		echo array_sum(range(1, 100));
-	echo '</div><br>';
-
-
-	
-//----------------------------------------------------TASK3                                                       
-	echo '<span>task3</span><div class="window">';
-		$arr = range(1, 9);
-		echo implode('<br>', $arr);
-	echo '</div><br>';
-	
-
-
-//----------------------------------------------------TASK4                                                       
-	echo '<span>task4. </span><div class="window">';
-		$arr = array_fill(0, 10, 'x');
-		var_dump($arr);
-	echo '</div><br>';
-	
-
-
-//----------------------------------------------------TASK5                                                       
-	echo '<span>task5</span><div class="window">';
-		$arr = range(1, 10);
-		shuffle($arr);
-		var_dump($arr);
-	echo '</div><br>';
-
-//----------------------------------------------------TASK6                                                       
-	echo '<span>task6. факториал числа не используя цикл</span><div class="window">';
-			$num = 5; //найдем факториал 5
-			$arr = range(1, $num);
-			echo array_product($arr);
-	echo '</div><br>';
-
-//----------------------------------------------------TASK7                                                       
-	echo '<span>task7.сумма цифр числа</span><div class="window">';
-			$num = 123;
-			$arr = str_split($num, 1);
-			echo array_sum($arr);
-	echo '</div><br>';
-
-
-//----------------------------------------------------TASK8                                                       
-	echo '<span>task8</span><div class="window">';
-		$str = 'abcde';
-		$str = strrev($str);
-		$str = ucfirst($str);
-		$str = strrev($str);
-		echo $str;
-	echo '</div><br>';
-
-	
-
-//----------------------------------------------------TASK9                                                       
-	echo '<span>task9.массив с квадратными корнями этих чисел не используя цикл</span><div class="window">';
-		$arr = array(1, 4, 5, 7, 9);
-		$result = array_map('sqrt', $arr);
-		var_dump($result);
-	echo '</div><br>';
-
-
-
-//----------------------------------------------------TASK10                                                       
-	echo '<span>task10.Массив c числами от 1 до 26 так, чтобы ключами этих чисел были буквы английского алфавита: ["a"=>1, "b"=>2...]</span><div class="window">';
-		$keys = range('a', 'z');
-		$elems = range(1, 26);
-		$arr = array_combine($keys, $elems);
-		var_dump($arr);
-	echo '</div><br>';
-
-//----------------------------------------------------TASK11                                                       
-	echo '<span>task11.сумма пар чисел: 12+34+56+78+90</span><div class="window">';
-		$str = '1234567890';
-		$arr = str_split($str, 2);
-		$result = array_sum($arr);
-		echo $result;
-	echo '</div><br>';
-
-
+	                   
+	<h3>Задачи на формы в PHP</h3><br><span>task1</span><div class="window">
+	<form action="" method="GET">
+		<input type="text" name="name1" placeholder="Введите имя">
+		<input type="submit">
+	</form> <br>
+<?php	
+		if (isset($_REQUEST['name1'])) {
+			$name = $_REQUEST['name1'];
+			echo 'Привет,'.$name;
+		}
 ?>
+	</div><br>
+
+
+<!--      -------- Task2-----------         -->
+	<span>task2</span>
+	<div class="window">
+		<form action="" method="GET">
+			<input type="text" name="name2" placeholder="Введите имя"><br>
+			<input type="text" name="age2" placeholder="Введите возраст"><br>
+			<textarea name="message2" placeholder="Ваше сообщение"></textarea>
+			<input type="submit" name="submit2">
+		</form>
+<?php	
+		if (isset($_REQUEST['submit2'])) {
+			$age = strip_tags($_REQUEST['age2']);
+			$name = strip_tags($_REQUEST['name2']);
+			$message = strip_tags($_REQUEST['message2']);
+			echo "Привет, $name, вам $age лет <br> твое сообщение: $message";
+		}
+?>
+	</div><br>
+
+<!--      -------- Task3-----------         -->
+<?php
+	if (!isset($_REQUEST['age3'])) {
+?>
+	<span>task3</span>
+	<div class="window">
+		<span>После ввода форма изчезнет</span><br>
+		<form action="" method="GET">
+			<input type="text" name="age3" placeholder="Ваш возраст">
+			<input type="submit">
+		</form>
+	
+<?php
+	}
+?>
+<?php
+	if (isset($_REQUEST['age3'])) {
+		$age = strip_tags($_REQUEST['age3']);
+		echo 'Ваш возраст: '.$age;
+	}
+?>
+	</div><br>
+
+<!--      -------- Task4-----------         -->
+	<span>task4</span>
+	<div class="window">
+		<form action="" method="GET">
+			<span>Введите логин 'user' и пароль: 123456</span><br>
+			<input type="text" name="login4" placeholder="логин"><br>
+			<input type="password" name="pass4" placeholder="пароль"><br>
+			<input type="submit" name="submit4"><br>
+		</form>
+<?php	
+		if (isset($_REQUEST['submit4'])) {
+			$login = 'user';
+			$pass = '123456';
+			$formLogin = trim($_REQUEST['login4']);
+			$formPass = trim($_REQUEST['pass4']);
+			if ($login == $formLogin and $pass == $formPass) {
+				echo 'Доступ разрешен!';
+			} else {
+				echo 'Нет доступа!';
+			}
+}?>
+	</div><br>
+	
+<!--      -------- Task5-----------         -->
+	<span>task5</span>
+	<div class="window">
+		<span>После ввода значение созраняется в форме</span><br>
+		<form action="" method="GET">
+			<input name="name5" value="<?php if (isset($_GET['name5'])) echo $_GET['name5']; ?>">
+			<input type="submit" name="submit5" >
+		</form>
+<?php	
+		if (isset($_REQUEST['submit5'])) {
+			$name = $_REQUEST['name5'];
+			echo $name;
+		}
+?>
+	</div><br>
+
+<!--      -------- Task6-----------         -->
+	<span>task6</span>
+	<div class="window">
+		<span>После ввода значения полей созраняются </span><br>
+		<form action="" method="GET">
+			<input name="name6" value="<?php if (isset($_GET['name6'])) echo $_GET['name6']; ?>"><br>
+			<textarea name="message6">
+			<?php if (isset($_GET['message6'])) echo $_GET['message6']; ?>
+			</textarea><br>
+			<input type="submit">
+		</form>
+
+	</div><br>
+
+
+
+
+
 </body>
 </html>
